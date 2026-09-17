de945cc28cb1502d090a140611b52d96ca8fc89d
JettNguyen <jett.nguyen@ufl.edu>
2026-09-16 20:50:01 -0400

    centre the time badge, and on narrow screens centre the hero and hand the captions from side to side. assets at 20260916-17.

--- js/glossary.js
+++ js/glossary.js
@@ -15,7 +15,7 @@ import {
   makeStartHere,
   makeBigPicture,
   makeJumpNav
-} from './glossary-card.js?v=20260916-16';
+} from './glossary-card.js?v=20260916-17';
 
 (function () {
   'use strict';

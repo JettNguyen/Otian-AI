8e971506de753048c0f1062290499357ab9ffe9a
JettNguyen <jett.nguyen@ufl.edu>
2026-09-16 20:53:30 -0400

    fade the phone's bottom out a little more on narrow screens. assets at 20260916-20.

--- js/glossary.js
+++ js/glossary.js
@@ -15,7 +15,7 @@ import {
   makeStartHere,
   makeBigPicture,
   makeJumpNav
-} from './glossary-card.js?v=20260916-19';
+} from './glossary-card.js?v=20260916-20';
 
 (function () {
   'use strict';

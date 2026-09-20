b902ba67aaec8d03cf43de9b02f70102048856aa
JettNguyen <jett.nguyen@ufl.edu>
2026-09-19 20:33:49 -0400

    stop the rim painting behind the phone. assets at 20260918-53.

--- js/glossary.js
+++ js/glossary.js
@@ -15,7 +15,7 @@ import {
   makeStartHere,
   makeBigPicture,
   makeJumpNav
-} from './glossary-card.js?v=20260918-52';
+} from './glossary-card.js?v=20260918-53';
 
 (function () {
   'use strict';

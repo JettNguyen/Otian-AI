89c24dd23273443b4a0e54698aa5fbe81bbff150
JettNguyen <jett.nguyen@ufl.edu>
2026-09-21 16:40:27 -0400

    let the quote and the name under it share one box

--- js/blog.js
+++ js/blog.js
@@ -8,7 +8,7 @@
    was by hand and by where its <script> sits. */
 import {
   makeFeatured, makeCard, normalizeArticles, escapeHtml,
-} from './blog-card.js?v=20260921-73';
+} from './blog-card.js?v=20260921-74';
 
 (function () {
   'use strict';
--- js/glossary.js
+++ js/glossary.js
@@ -15,7 +15,7 @@ import {
   makeStartHere,
   makeBigPicture,
   makeJumpNav
-} from './glossary-card.js?v=20260921-73';
+} from './glossary-card.js?v=20260921-74';
 
 (function () {
   'use strict';

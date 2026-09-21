313ae88795a150c0ed36fc3d495bf3bb0c6f61ba
JettNguyen <jett.nguyen@ufl.edu>
2026-09-21 16:23:28 -0400

    give the three add-on kinds their colors in the parts list

--- js/glossary.js
+++ js/glossary.js
@@ -15,7 +15,7 @@ import {
   makeStartHere,
   makeBigPicture,
   makeJumpNav
-} from './glossary-card.js?v=20260921-71';
+} from './glossary-card.js?v=20260921-72';
 
 (function () {
   'use strict';
